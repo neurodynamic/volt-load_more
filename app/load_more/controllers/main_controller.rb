d9eea0d5cc4d86edd9c20498054bb8294c309a28
neurodynamic <neurodynamicdev@gmail.com>
2015-07-06 15:49:41 -0400

    ensure variables are integers before incrementing variable

--- app/load_more/controllers/main_controller.rb
+++ app/load_more/controllers/main_controller.rb
@@ -2,7 +2,7 @@ module LoadMore
   class MainController < Volt::ModelController
 
     def load_more(event)
-      attrs.current_limit += attrs.increment_by.to_i
+      attrs.current_limit = attrs.current_limit.to_i + attrs.increment_by.to_i
     end
   end
 end
--- lib/volt/load_more/version.rb
+++ lib/volt/load_more/version.rb
@@ -1,5 +1,5 @@
 module Volt
   module LoadMore
-    VERSION = "0.1.1"
+    VERSION = "0.1.2"
   end
 end
